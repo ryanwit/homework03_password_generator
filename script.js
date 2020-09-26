dddf69e936b52fc38ebab7d9cdfd608b7353ee1b
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-09-25 18:33:35 -0600

    Updated if statements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // ---------------GLOBAL VARIABLES----------------------------//
-var upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   var lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var number: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
   var symbols: ["!", "#", "%", "&", "(", ")", "*", "+", "-", "~", "/", ":", "<", ">", "?", "^", "[", "]", "{", "}"];
@@ -13,45 +13,62 @@ var upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
   //   userNumbers: [],
   
 
-  // ---------------FUNCTIONS----------------------------//
+  // -------------------------------FUNCTIONS--------------------------------//
 
   function getCriteria() {
       // console.log{confirmLength}
-      var confirmLength = prompt("How long would you like your password to be? Between 8 and 128 Characters")
-      var confirmUpper = prompt ("What uppercase character would you like?")
-
-        var upper = uppers [uppers.indexOf(confirmUpper)]
-        array.push(upper)
-
-      var confirmLower = prompt ("What lowercase character would you like?")
-
-        var confirmLower = lowers [lowers.indexOf(confirmLower)]
-        array.push(lower)
-
-      var confirmSpecial = prompt ("What special character would you like?")
-      
-        var special = specials [specials.indexOf(confirmSpecial)]
-        array.push(special)
-
-      var confirmNumber = prompt ("What number would you like?")
-
-        var number = numbers [numbers.indexOf(confirmNumber)]
-        array.push(number)
-
-            console.log(array);
-
-            console.log(confirmLength);
-            console.log(upper);
-            console.log(lower);
-            console.log(special);
-            console.log(number);
-
-        if (+confirmLength > 8 || +confirmLength < 128) {
-
-
+      var confirmLength = prompt("How long would you like your password to be? Select between 8 and 128 Characters")
+          if (confirmLength < 8) || confirmLength > 128) {
+            alert("PLEASE CHOOSE A NUMBER BETWEEN 8 AND 128")
+            return
+          }
         }
+        if (isNaN(confirmLength)===) {
+          alert("PASSWORD LENGTH MUST BE PROVDED AS A NUMBER")
+          return
+        }
+  } 
+      var confirmUpper = confirm ("Would you like your password to contain uppercase letters?")
+      var confirmLower = confirm ("Would you like your password to contain lowercase letter?")
+      var confirmNumber = confirm ("Would you like your password to contain number(s)")
+      var confirmSymbol = confirm ("WOuld  you like your password to contain special characters?")
+      if (confirmUpper === fales && confirmLower === fales && confirmNumber === false && confirmSymbol === false) {
+        alert("YOU MUST CHOOSE AT LEAST ONE PARAMETER LISTED")
+    }
 
+      var passwordSelectedCriteria = {
+        confirmLength,
+        confirmUpper,
+        confirmLower,
+        confirmNumber,
+        confirmSymbol,
       }
+        return passwordSelectedCriteria
+    }
+      function randomFunction(arr) {
+        var randomIndex = Math.floor(Math.random()*arr.length)
+        var randomElement = arr[randomIndex]
+        return randomElement
+      }
+
+      function generatePassword() {
+        var options = getCriteria()
+        var result = []
+        var possibleChar = []
+        var guaranteedChar = []
+        if (options.confirmSymbol === true) {
+          possibleChar=possibleChar.concat(symbols)
+          guaranteedChar.push(randomFunction(symbols))
+        }
+      }
+
+
+
+
+
+   
+
+      
 
   
 
